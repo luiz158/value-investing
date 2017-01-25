d2ecce86aff6b9cdb2969fbf92b4e2a368e3a79c
Victor Dolirio <victor.barbosa@tst.jus.br>
2017-01-25 18:19:31 -0200

    Commit inicial de extensão do chrome para ajudar nas análises fundamentalistas.

---
+++ content.js
@@ -0,0 +1,75 @@
+//Funções
+var executaChecklist,
+    buscarVolumeDiario21D,
+    buscarValorDeMercado,
+    buscarNivelGovernanca,
+    buscarLiquidezCorrente,
+    buscarROE,
+    buscarDividaBrutaSobrePatrimonioLiquido,
+    buscarPrecoSobreValorPatrimonial,
+    buscarPrecoSobreLucro;
+
+//Modelo de domínio
+var dados = {
+    volumeDiarioMedio21d: null,
+    valoresDeMercado: null,
+    nivelGovernanca: null,
+    liquidezCorrente: null,
+    retornoSobrePatrimonioLiquido: null,
+    dividaBrutaSobrePatrimonioLiquido: null,
+    precoSobreValorPatrimonial: null,
+    precoSobreLucro: null
+};
+
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    if( request.message === "clicked_browser_action" ) {
+      executaChecklist();
+      console.log(dados);
+    }
+  }
+);
+
+executaChecklist = function() {
+    dados.volumeDiarioMedio21d = buscarVolumeDiario21D();
+    dados.valoresDeMercado = buscarValorDeMercado();
+    dados.nivelGovernanca = buscarNivelGovernanca();
+    dados.liquidezCorrente = buscarLiquidezCorrente();
+    dados.retornoSobrePatrimonioLiquido = buscarROE();
+    dados.dividaBrutaSobrePatrimonioLiquido = buscarDividaBrutaSobrePatrimonioLiquido();
+    dados.precoSobreValorPatrimonial = buscarPrecoSobreValorPatrimonial();
+    dados.precoSobreLucro = buscarPrecoSobreLucro();
+
+}
+
+buscarVolumeDiario21D = function() {
+    return $("#lbVolumeMedio").html();
+}
+
+buscarValorDeMercado = function() {
+    return $("#lbValorMercadoAtual").html();
+}
+
+buscarNivelGovernanca = function() {
+    return $("#lbGovernanca").html();
+}
+
+buscarLiquidezCorrente = function() {
+    return $("#lbLiquidezCorrente3").html();
+}
+
+buscarROE = function() {
+    return "<<a implementar>>";
+}
+
+buscarDividaBrutaSobrePatrimonioLiquido = function() {
+    return $("#lbDividaTotalBrutaSobrePatrimonioLiquido3").html();
+}
+
+buscarPrecoSobreValorPatrimonial = function() {
+    return $("#lbPrecoValorPatrimonialAtual").html();
+}
+
+buscarPrecoSobreLucro = function() {
+    return $("#lbPrecoLucroAtual").html();
+}
